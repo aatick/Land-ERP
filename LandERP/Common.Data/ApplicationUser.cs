51e254a4896dc78f42b5f29d330257c39e438e9c
aatick <aatick.ru3@gmail.com>
2019-08-22 13:33:07 +0600

    Add files via upload

---
+++ LandERP/Common.Data/ApplicationUser.cs
@@ -0,0 +1,38 @@
+﻿using Common.Data.CommonDataModel;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common.Data
+{
+    public class ApplicationUser : IdentityUser
+    {
+        public ApplicationUser()
+        {
+            DateCreated = DateTime.Now;
+        }
+
+        public string FirstName { get; set; }
+        public int UserId { get; set; }
+        public string LastName { get; set; }
+
+        public string ProfilePicUrl { get; set; }
+
+        public DateTime DateCreated { get; set; }
+
+        public DateTime? LastLoginTime { get; set; }
+
+        public bool Activated { get; set; }
+
+        public int RoleId { get; set; }
+
+        public string DisplayName
+        {
+            get { return FirstName + " " + LastName; }
+        }
+        // public virtual Employee Employee { get; set; }
+    }
+}
